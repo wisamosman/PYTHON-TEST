40af30a39d8216da0e6cc712f9ae0a8ff6fa02e6
wisamosman <viesam_1987@hotmail.com>
2023-02-24 23:52:41 +0100

    edit first

--- first pro.py
+++ first pro.py
@@ -1,13 +1,22 @@
-5+4
-9
-x = 1
-y = 2
-print(x+y)
-5-2
-print(x*y)
-print(y/x)
-x = 12
-y = 13
-print(x+y)
-
-
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x = False
+y = 5
+z = 2.5
+s = "wesam"
+print(y-z)
+2.5
+>>> no
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    no
+NameError: name 'no' is not defined
+>>> g = [1,2,3,4,5]
+>>> t = (10,11,12,13,14,15)
+>>> g = (1,2,3,4,5)
+>>> d = {'wesam' :10 , 'ali' :20 , 'ahmad' :30}
+>>> no
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    no
+NameError: name 'no' is not defined
