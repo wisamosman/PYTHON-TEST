150bfc71ac97aed2b15b57bc24fad6e8617bdea3
wisamosman <viesam_1987@hotmail.com>
2023-03-18 23:46:48 +0100

    add html5

--- first pro.py
+++ first pro.py
@@ -1,21 +1,7 @@
-
-x=5
-if x >3:
+x=10
+if x<100:
+    print('ok')
+    if x==10:
+        print('yes')
+else:
     print('wesam')
-else:
-        print('erorr')
-y=7
-if y<2:
-   print('ok')
-else:
-   print('no')
-    
-q=10
-if q<9:
-    print('ok9')
-elif q>8:
-    print('ok8')
-elif q==100:
-    print('q=100')
-else:
-    print('no')
