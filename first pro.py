dfae8cf843118adeb29f109b088593599b27b2c2
wisamosman <viesam_1987@hotmail.com>
2023-03-19 00:56:56 +0100

    add html28

--- first pro.py
+++ first pro.py
@@ -1,5 +1,6 @@
 
-start=int(input('enter start :'))
-end=int(input('enter end :'))
-for x in range (start,end):
-    print(x,x*x)
+rows=int(input('enter rows :'))
+cols=int(input('enter cols:'))
+for x in range (rows):
+    for y in range (cols):
+        print('*')
