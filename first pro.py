22cd482b6ed90f0844c5d220c4534087110c09fb
wisamosman <viesam_1987@hotmail.com>
2023-03-18 23:52:15 +0100

    add html8

--- first pro.py
+++ first pro.py
@@ -1,13 +1,2 @@
 x=10
-if x<100:
-    print('ok')
-    if x==10:
-        print('yes')
-else:
-    print('wesam')
-y=10
-q=2
-if y==10 or q==2:
-    print('ok')
-else:
-    print('no')
+print('ok') if x==10 else print('no')
