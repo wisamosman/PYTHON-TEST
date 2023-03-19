d2e1c26db5b33a6427a09bb9a150c87541c7fb84
wisamosman <viesam_1987@hotmail.com>
2023-03-19 11:09:52 +0100

    add html36

--- first pro.py
+++ first pro.py
@@ -1,8 +1,11 @@
-class ahmad:
-    def ibr(self,x,y):
+class wesam:
+    def ahmad(self,x,y):
+        print(x+y)
+    def ibr (self,x,y):
         print(x-y)
-    def ays(self,x,y):
-        print(x/y)
-c1=ahmad( )
-c1.ibr(9,4)
-c1.ays(9,3)
+    def ays (self,x,y):
+        print(x*y)
+c1=wesam()
+c1.ahmad(3,7)
+c1.ibr(9,3)
+c1.ays(2,4)
