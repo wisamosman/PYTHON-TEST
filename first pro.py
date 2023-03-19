816727e60cb9aee86c29a5e104ceae5884775294
wisamosman <viesam_1987@hotmail.com>
2023-03-19 11:40:20 +0100

    add html38

--- first pro.py
+++ first pro.py
@@ -1,13 +1,2 @@
-class wesam:
-    def ahmad(self,x,y):
-        print(x+y)
-    def ibr (self,x,y):
-        print(x-y)
-    def ays (self,x,y):
-        print(x*y)
-c1=wesam()
-c1.ahmad(3,7)
-c1.ibr(9,3)
-c1.ays(2,4)
-def __init__(self) :
- print('welcom')
+
+    
