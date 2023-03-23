fe050528759f995b1ec41de2aaf6990d355f2ab2
wisamosman <viesam_1987@hotmail.com>
2023-03-23 22:41:36 +0100

    add html24

--- first pro.py
+++ first pro.py
@@ -1,36 +1,11 @@
-
-class Wesam :
+class wesam:
     def __init__(self,name):
         print(f'welcom {name}')
-    def ahmad (self,x,y):
+    def wesso(self,x,y):
         print(x+y)
-    def ibo (self,x,y):
+    def ibo(self,x,y):
         print(x*y)
-
-c1=Wesam('ayshe' )
-c1.ahmad(3,5)
-c1.ahmad(6,19)
-c1.ahmad(55,78)
-c1.ibo(6,7)
-c1.ibo(8,4)
-class hsso :
-     def __init__(self,name):
-         print(f'welcom {name}')
-     def ahmad (self,x,y):
-         print(x-y)
-     def ibo (self, x,y):
-         print(x+y)
-c2=hsso('wesam1' )
-c2.ahmad(44,55)
-c2.ibo(55,73)
-class dado:
-    def __init__(self,name):
-        print(f'welcom {name}')
-    def ahmd(self,x,y):
-        print(x+y)
-    def mama(self,x,y):
-         print(x-y)
-c3=dado('meryam')
-c3.ahmd(87,37)
-c3.mama(73,909)
+c1=wesam('ayshe')
+c1.wesso(63,22)
+c1.ibo(22,23)
 
