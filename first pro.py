8064be657ad4d6858e6736fdfab2b29e19ac9b11
wisamosman <viesam_1987@hotmail.com>
2023-03-23 22:48:07 +0100

    add html24

--- first pro.py
+++ first pro.py
@@ -9,3 +9,13 @@ c1=wesam('ayshe')
 c1.wesso(63,22)
 c1.ibo(22,23)
 
+class Mama :
+    def __init__(self,name):
+        print(f'welcom {name}')
+    def ahhmo(self,x,y):
+        print(x+y)
+    def ibr(self,x,y):
+        print(x,y)
+c2=Mama('wesamo')
+c2.ahhmo(72,22)
+c2.ibr(22,67)
