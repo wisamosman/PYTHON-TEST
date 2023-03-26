e50ef1918cf9094bfa77cd90ea4923a465af048b
wisamosman <viesam_1987@hotmail.com>
2023-03-26 22:08:31 +0200

    add html24

--- first pro.py
+++ first pro.py
@@ -187,6 +187,9 @@ class roe :
         self.balance -=account
         print (f'your balance is :{self.balance}')
 
+    def mop(self,account):
+        self.balance +=account
+        print(f'your balance is :{self.balance}')
 c5=roe('wesso',38)
 c5.dop(150)
 
